1bc8148d7c216ababa58e9ca9ea7508c95755316
GianellaAlvarez <vgilalvarez8@gmail.com>
2023-09-29 00:02:31 -0500

    Añadi el css de los archivos que faltaban, también las imagenes y el js que utilice para el archivo de normas.html

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// script.js
+document.getElementById("opcion1").addEventListener("click", function () {
+    mostrarContenido("info1");
+});
+
+document.getElementById("opcion2").addEventListener("click", function () {
+    mostrarContenido("info2");
+});
+
+document.getElementById("opcion3").addEventListener("click", function () {
+    mostrarContenido("info3");
+});
+
+document.getElementById("opcion4").addEventListener("click", function () {
+    mostrarContenido("info4");
+});
+
+document.getElementById("opcion5").addEventListener("click", function () {
+    mostrarContenido("info5");
+});
+
+function mostrarContenido(idContenido) {
+    // Ocultar todos los contenidos
+    document.getElementById("info1").style.display = "none";
+    document.getElementById("info2").style.display = "none";
+    document.getElementById("info3").style.display = "none";
+    document.getElementById("info4").style.display = "none";
+    document.getElementById("info5").style.display = "none";
+
+
+    // Mostrar el contenido seleccionado
+    document.getElementById(idContenido).style.display = "block";
+}
+
+// Mostrar el contenido inicial (por ejemplo, Opción 1)
+mostrarContenido("info1");
